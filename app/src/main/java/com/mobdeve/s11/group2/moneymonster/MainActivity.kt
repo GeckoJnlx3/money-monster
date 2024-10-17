54cb2f1e4a88b1f221ae132796b738d9b31d4783
Alaine <103810781+GeckoJnlx3@users.noreply.github.com>
2024-10-17 12:27:25 +0800

    chore. removed unnecessary methods on MainActivity.kt

--- app/src/main/java/com/mobdeve/s11/group2/moneymonster/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group2/moneymonster/MainActivity.kt
@@ -16,32 +16,6 @@ import com.mobdeve.s11.group2.moneymonster.ui.theme.MoneyMonsterTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MoneyMonsterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoneyMonsterTheme {
-        Greeting("Android")
     }
 }
