e6824631e5f05a509bf69c9052a92e33b60c3cd9
GeckoJnlx3 <103810781+GeckoJnlx3@users.noreply.github.com>
2024-10-18 23:12:12 +0800

    chore. refactored for the renamed layouts

--- app/src/main/java/com/mobdeve/s11/group2/moneymonster/RegisterActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group2/moneymonster/RegisterActivity.kt
@@ -2,7 +2,6 @@ package com.mobdeve.s11.group2.moneymonster
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
@@ -11,7 +10,7 @@ class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.register_main)
+        setContentView(R.layout.register)
 
         var loginBtn: TextView = findViewById(R.id.registerLogInTv)
         loginBtn.setOnClickListener(){
