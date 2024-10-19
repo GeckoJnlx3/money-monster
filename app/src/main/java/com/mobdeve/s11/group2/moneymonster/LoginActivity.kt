f7cd97c19d2291e96c3cf99a3c6d1d315993dcf2
GeckoJnlx3 <103810781+GeckoJnlx3@users.noreply.github.com>
2024-10-19 11:48:12 +0800

    refac. used viewBinding instead of findViewById()

--- app/src/main/java/com/mobdeve/s11/group2/moneymonster/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group2/moneymonster/LoginActivity.kt
@@ -4,20 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import com.mobdeve.s11.group2.moneymonster.databinding.LoginBinding
 
 class LoginActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var viewBinding: LoginBinding = LoginBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
-        setContentView(R.layout.login)
-
-        var registerBtn: TextView = findViewById(R.id.loginRegisterNowTv)
-        registerBtn.setOnClickListener(){
+        var registerTv: TextView = viewBinding.loginRegisterNowTv
+        registerTv.setOnClickListener(){
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-        var enterBtn: TextView = findViewById(R.id.loginEnterBtn)
+        var enterBtn: TextView = viewBinding.loginEnterBtn
         enterBtn.setOnClickListener(){
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/mobdeve/s11/group2/moneymonster/RegisterActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group2/moneymonster/RegisterActivity.kt
@@ -2,19 +2,26 @@ package com.mobdeve.s11.group2.moneymonster
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import com.mobdeve.s11.group2.moneymonster.databinding.RegisterBinding
 
 class RegisterActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var viewBinding: RegisterBinding = RegisterBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
-        setContentView(R.layout.register)
-
-        var loginBtn: TextView = findViewById(R.id.registerLogInTv)
+        var loginBtn: TextView = viewBinding.registerLogInTv
         loginBtn.setOnClickListener(){
             val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        var enterBtn: Button = viewBinding.registerBtn
+        enterBtn.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
