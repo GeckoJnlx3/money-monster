c54f70c4025d3913dac02e964f0f9b96a50fe0cf
W-Choudai <daniellemariempua5@gmail.com>
2024-10-25 17:58:24 +0800

    added setting spinner content

--- app/src/main/java/com/mobdeve/s11/group2/moneymonster/SettingsActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group2/moneymonster/SettingsActivity.kt
@@ -2,22 +2,34 @@ package com.mobdeve.s11.group2.moneymonster
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.mobdeve.s11.group2.moneymonster.databinding.SettingsBinding
 
 class SettingsActivity : ComponentActivity() {
 
-    private lateinit var loginBtn: Button
+    private lateinit var binding: SettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings)
+        binding = SettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        loginBtn = findViewById(R.id.loginBtn)
+        val currencySpinner: Spinner = binding.currencySpnr
+        val timeSpinner: Spinner = binding.timeSpnr
 
-        loginBtn.setOnClickListener {
+        val currencies = arrayOf("USD", "EUR", "JPY", "GBP", "AUD")
+        val currencyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencies)
+        currencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        currencySpinner.adapter = currencyAdapter
+
+        val timeOptions = arrayOf("Daily", "Weekly", "Monthly", "Yearly")
+        val timeAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, timeOptions)
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        timeSpinner.adapter = timeAdapter
+
+        binding.loginBtn.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
