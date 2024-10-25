f023bc00b76eebb91ffb1f5f89621b90914fac89
GeckoJnlx3 <103810781+GeckoJnlx3@users.noreply.github.com>
2024-10-25 22:35:16 +0800

    feat. added datePickerDialog to show the user the dates the user can pick

--- app/src/main/java/com/mobdeve/s11/group2/moneymonster/FinanceActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group2/moneymonster/FinanceActivity.kt
@@ -1,5 +1,6 @@
 package com.mobdeve.s11.group2.moneymonster
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Bundle
@@ -11,15 +12,18 @@ import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.res.colorResource
+import androidx.compose.material3.AlertDialog
+import com.mobdeve.s11.group2.moneymonster.databinding.FinanceBinding
+import java.util.Calendar
 
 class FinanceActivity : ComponentActivity() {
 
     private lateinit var logExpenseBtn: Button
     private lateinit var logIncomeBtn: Button
     private lateinit var typeText: TextView
+    private lateinit var dateEt: EditText
     private lateinit var amountInput: EditText
-    private lateinit var descriptionInput: EditText
+    private lateinit var memoInput: EditText
     private lateinit var saveBtn: Button
     private lateinit var categorySpnr: Spinner
     private lateinit var imageView: ImageView
@@ -39,17 +43,18 @@ class FinanceActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.finance)
+        val viewBinding: FinanceBinding = FinanceBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
-        logExpenseBtn = findViewById(R.id.logExpenseBtn)
-        logIncomeBtn = findViewById(R.id.logIncomeBtn)
-        typeText = findViewById(R.id.typeText)
-        amountInput = findViewById(R.id.amountInput)
-        descriptionInput = findViewById(R.id.memoInput)
-        saveBtn = findViewById(R.id.saveBtn)
-        imageView = findViewById(R.id.imageView)
-        categorySpnr = findViewById(R.id.categorySpnr)
-        currencyText = findViewById(R.id.currencyText)
+        logExpenseBtn = viewBinding.logExpenseBtn
+        logIncomeBtn = viewBinding.logIncomeBtn
+        typeText = viewBinding.typeText
+        dateEt = viewBinding.dateEt
+        amountInput = viewBinding.amountInput
+        memoInput = viewBinding.memoInput
+        saveBtn = viewBinding.saveBtn
+        imageView = viewBinding.imageView
+        categorySpnr = viewBinding.categorySpnr
 
         val categoryAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -67,6 +72,10 @@ class FinanceActivity : ComponentActivity() {
 
         saveBtn.setOnClickListener {
             saveTransaction()
+        }
+
+        dateEt.setOnClickListener {
+            displayDatePickerDialog()
         }
 
         loadCurrency()
@@ -96,18 +105,19 @@ class FinanceActivity : ComponentActivity() {
             else
                 "Log Income"
         amountInput.setText("")
-        descriptionInput.setText("")
         currencyText.text = currency
+        memoInput.setText("")
+
     }
 
     private fun saveTransaction() {
         val amount = amountInput.text.toString().toDoubleOrNull()
-        val description = descriptionInput.text.toString()
+        val description = memoInput.text.toString()
 
         if (amount != null) {
 
             amountInput.setText("")
-            descriptionInput.setText("")
+            memoInput.setText("")
 
             imageView.setImageResource(
                 if (isLoggingExpense)
@@ -133,5 +143,27 @@ class FinanceActivity : ComponentActivity() {
         val sharedPref = getSharedPreferences("com.mobdeve.s11.group2.moneymonster.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE)
         currency = sharedPref.getString("CURRENCY", "PHP") ?: "PHP"
         currencyText.text = currency
+    }
+
+    private fun displayDatePickerDialog(){
+        // https://www.geeksforgeeks.org/how-to-popup-datepicker-while-clicking-on-edittext-in-android/
+        val c = Calendar.getInstance()
+
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { view, year, monthOfYear, dayOfMonth ->
+                val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                dateEt.setText(dat)
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
     }
 }
