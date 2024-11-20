b6b1e4ad5360f664daf7705b4d031c1451c82a49
W-Choudai <daniellemariempua5@gmail.com>
2024-11-21 01:57:47 +0800

    Moved/Addded Date range spinners

--- app/src/main/java/com/mobdeve/s11/group2/moneymonster/HistoryActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group2/moneymonster/HistoryActivity.kt
@@ -1,18 +1,28 @@
 package com.mobdeve.s11.group2.moneymonster
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class HistoryActivity : ComponentActivity() {
 
+    private lateinit var dateRangeSpinner2: Spinner
     private lateinit var recyclerView: RecyclerView
     private lateinit var databaseHelper: FinanceDatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.history)
+
+        dateRangeSpinner2 = findViewById(R.id.dateRangeSpnr2)
+
+        val weekRanges = generateWeekRanges()
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, weekRanges)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        dateRangeSpinner2.adapter = spinnerAdapter
 
         databaseHelper = FinanceDatabaseHelper(this)
 
@@ -30,5 +40,15 @@ class HistoryActivity : ComponentActivity() {
 
         val adapter = HistoryRecordDateAdapter(groupedByDateAndType)
         recyclerView.adapter = adapter
+    }
+
+    private fun generateWeekRanges(): List<String> {
+        return listOf(
+            "October 1 - October 7",
+            "October 8 - October 14",
+            "October 15 - October 21",
+            "October 22 - October 28",
+            "October 29 - November 4"
+        )
     }
 }
